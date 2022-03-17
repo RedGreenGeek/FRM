0535f274639997482dcdd0d42ae24bcdcb11b97b
Andreas <andreas@engly.eu>
2022-03-17 10:53:48 +0100

    Opdaterede funktioner

--- Trash/Functions.py
+++ Trash/Functions.py
@@ -3,21 +3,117 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
-import statistics
 import math
 from scipy.stats import norm
 from scipy.stats import chi2
+import datetime as dt
+import seaborn as sns
 
+###### Preparing for load bar ########
+
+# Some of the functions might take long to execute. 
+# Therefore, a bar has been incorporated.
 
 ###### DEFINING FUNCTIONS ######
+
+### Functions for converting to dates ###
+
+def descriptive_statistics(df, trading_days = 247):
+
+    length = df.shape[0]
+    Results = {}
+    Years = length/trading_days
+
+    for column in df.columns:
+
+        if column not in ['Day', 'Month', 'Year']:
+
+            AR = round(((df[column].values[length-1]/df[column].values[0])**(1/Years) - 1)*100, 2)
+            SD = round(np.std(df[column].pct_change()[1:]) * np.sqrt(trading_days)*100, 2)
+            TR = round((df[column].iloc[length-1]/df[column].iloc[0]-1)*100, 2)
+            SR = round(AR/SD,2)
+
+            Results[column] = {
+                'Annualized return': AR ,
+                'Standard deviation': SD,
+                'Total return': TR,
+                'Sharp ratio':  SR
+            }
+
+    return(Results)
+
+def correlation_matrix(df):
+
+    SIGMA = df.corr()
+    sns.heatmap(SIGMA)
+    plt.savefig('covariance_matrix.svg', dpi=2400)
+
+
+def convert_to_dates(df):
+
+    length = df.shape[0]
+    Dates = []
+
+    for i in range(0,length):
+
+        date_as_string = str(df['Day'].iloc[i]) + "/" + str(df['Month'].iloc[i]) + "/" + str(df['Year'].iloc[i])
+        Dates.append(dt.datetime.strptime(date_as_string,'%d/%m/%Y').date())
+
+    return (pd.DataFrame(Dates, columns = ['Date']))
+
+
+### Conversion of base currency ###
+
+def FX_conversion(stock, FX_rate):
+
+    # Find number of rows
+    length = stock.shape[0] 
+
+    # Results
+    converted_rates = np.zeros(length)
+
+    # Run conversion new
+    for i in range(0,length):
+        converted_rates[i] = stock.iloc[i] * FX_rate.iloc[i]
+
+    return (converted_rates)
+    
+# Converting bunch of stocks
+
+def FX_conversions(stocks_FX, pairing):
+
+    converted_stocks = pd.DataFrame()
+    converted_stocks['Year'] = stocks_FX['Year']
+    converted_stocks['Month'] = stocks_FX['Month']
+    converted_stocks['Day'] = stocks_FX['Day']
+
+    for stock in pairing:
+
+        # check if base
+
+        if pairing[stock] == 'Original':
+
+            col_name = 'USD'
+            conversion = stocks_FX[stock]
+
+        else:
+
+            col_name = pairing[stock][3:6]
+            conversion = FX_conversion(stocks_FX[stock], stocks_FX[pairing[stock]])
+
+        # Save result
+        converted_stocks[stock + "." + col_name] = conversion
+
+    return (converted_stocks)
 
 
 def exp_weights(lamb, i, n):
 
+    # Cannot take lambda = 1
+
     return(((lamb**(n-i))*(1-lamb)/(1-lamb**n)))
 
-def compute_returns(levels):
+def compute_return(levels):
     
     returns = [0]
     
@@ -26,6 +122,62 @@ def compute_returns(levels):
         returns.append((levels[i+1] - levels[i])/levels[i])
         
     return(returns)
+
+def compute_returns(df):
+
+    assets = df.columns
+    n_days = df.shape[0]
+
+    df_returns = pd.DataFrame()
+    
+    for asset in assets:
+        df_returns[asset] = compute_return(list(df[asset]))
+        
+    return(df_returns.iloc[1:,:])
+
+# This function used EWMA to estimate the asset-wise variance
+# I wonder why they do not use the entire covariance matrix?
+
+def EWMA_SIGMA_NO_CORRELATION(df, lambdA, n_days):
+
+    # It returns a vector of variances, if k_assets > 1.
+    # Else it returns a single number representing the portfolio variance
+    
+    # Limits
+    max_days = df.shape[0]
+    k_assets = df.shape[1]
+    
+    # Select last n days
+    if max_days <= n_days:
+        n_days = max_days
+    
+    df = df.tail(n_days).copy()
+
+    if k_assets == 1:
+
+        # Initializing SIGMA as variance of portfolio
+        SIGMA = pd.DataFrame.var(df).values[0]
+
+        # Estimating covariance matrix with EWMA
+        for i in range(0, n_days):
+            
+            # Handle SIGMA as a real number
+            r = df.iloc[i].values[0]
+            SIGMA = lambdA * SIGMA + (1 - lambdA) * (r**2)
+    
+    else:
+
+        # Initializing SIGMA as variance of columns for asset-wise approach
+        SIGMA = pd.DataFrame.var(df).values
+
+        # Estimating covariance matrix with EWMA
+        for i in range(0, n_days):
+            
+            # Handle SIGMA as an array
+            r = df.iloc[i].values
+            SIGMA = lambdA * SIGMA + (1 - lambdA) * np.power(r, 2)
+
+    return SIGMA
 
 def EWMA_SIGMA(df, lambdA, n_days):
     
@@ -54,7 +206,7 @@ def EWMA_SIGMA(df, lambdA, n_days):
 def Model_Based_Approach(df, allocation, 
                          n_days = 250, 
                          EWMA = False, 
-                         lambdA = 0.94, 
+                         lambdA = 0.995, 
                          verbose = False, 
                          quantiles = [95, 99]):
     
@@ -95,7 +247,7 @@ def Model_Based_Approach(df, allocation,
         print("--------- Analysis by the Model-based Approach --------- \n")
         print("    * Days used: ", n_days)
         print("    * EWMA: ", EWMA)
-        print("    * lambda: ", lambdA if EWMA else "NaN", "\n")
+        print("    * lambda: ", lambdA if EWMA else "Not relevant as EWMA is not used", "\n")
         
         for key in metrics:
             
@@ -107,7 +259,7 @@ def Model_Based_Approach(df, allocation,
 
 def Historical_Simulation(df, allocation, 
                          n_scenarios = 250, 
-                         weighted = False, 
+                         weight_scheme = False, 
                          lambdA = 0.94, 
                          verbose = False, 
                          quantiles = [95, 99]):
@@ -120,8 +272,10 @@ def Historical_Simulation(df, allocation,
     # Select last n days
     df = df.tail(n_scenarios).copy()
     
-    if weighted:
+    # If exponential decay, then compute weights. Otherwise just weight them equally.
+    if weight_scheme:
         weights = [exp_weights(lambdA, i, n_scenarios) for i in range(1, n_scenarios + 1)]
+
     else:
         weights = [1/n_scenarios for i in range(0,n_scenarios)]
         
@@ -144,7 +298,7 @@ def Historical_Simulation(df, allocation,
     accumulated_weights = [sum(df_sorted['Weights'].iloc[0:i]) for i in range(1, n_scenarios + 1)]
     
     df_sorted['ACC_Weights'] = accumulated_weights
-    
+
     # Saving the result
     results = {}
 
@@ -153,27 +307,154 @@ def Historical_Simulation(df, allocation,
         
         i = 0
         
-        for i in range(0,n_scenarios):
+        for i in range(0,n_scenarios-1):
             
             if df_sorted['ACC_Weights'].iloc[i] > (1-quantile):
                 
                 if i > 0:  
-                    results['VaR-' + str(int(quantile * 100))] = -df_sorted['Loss'].iloc[i-1]
-                    results['ES-' + str(int(quantile * 100))] = -np.mean(df_sorted['Loss'].iloc[0:i])
+                    results['VaR-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[i-1], 2)
+                    results['ES-' + str(int(quantile * 100))] = round(-np.mean(df_sorted['Loss'].iloc[0:i]), 2)
                     break
                     
                 else:  
-                    results['VaR-' + str(int(quantile * 100))] = -df_sorted['Loss'].iloc[0]
-                    results['ES-' + str(int(quantile * 100))] = -df_sorted['Loss'].iloc[0]
+                    results['VaR-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[0], 2)
+                    results['ES-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[0], 2)
+                    break
+
+    if verbose:
+        print(results)
+
+    return results
+
+
+# Volatility scaled losses on
+def Historical_Simulation_Volatility_Adjusted(df, allocation, 
+                         n_scenarios = 252, 
+                         n_volatility = 252,
+                         weight_scheme = False, 
+                         portfolio_wise = False,
+                         lambdA = 0.94, 
+                         EWMA_factor = 0.995,
+                         verbose = False, 
+                         quantiles = [95, 99]):
+
+    # Total investment
+    total_investment = np.sum(allocation)
+
+    # Specify important dimension
+    n_observations = df.shape[0]
+    n_assets = df.shape[1]
+
+    # If n_scenario + n_volatility is larger than the total dataset, then the number of scenarios is scaled accordingly.
+    if (n_scenarios + n_volatility) >= n_observations:
+        n_scenarios = n_observations - n_volatility
+        print("We are not able to run the simulation, since the number of scenarios and number")
+        print("of days to estimate volatility exceeds the total number of observations.\n")
+        print("The adjusted number of scenarios is: " + str(n_scenarios))
+    
+    # In order to get n scenarios, we must use n + 1 days
+
+    # Definition of scenario i
+    # scenario_i = (v_i / v_(i-1) - 1)
+    
+    # Select last n_scenarios + n_volatilities s.t. volatility can be computed on the first scenario
+    df_most_recent = df.tail(n_scenarios + n_volatility).copy()
+    df_results = df.tail(n_scenarios).copy()
+
+    # If exponential decay, then compute weights. Otherwise just weight them equally.
+    if weight_scheme:
+        weights = [exp_weights(lambdA, i, n_scenarios) for i in range(1, n_scenarios + 1)]
+
+    else:
+        weights = [1/n_scenarios for i in range(0,n_scenarios)]
+        
+    # Calculate loss on in all scenarios
+    adjusted_loss = []
+
+    if not portfolio_wise:
+
+        print("The program is calculating VaR and ES on asset-by-asset approach!\n")
+
+        # Calculate the estimate for the volatility on the day after the last scenario
+        # That is use the scenarios up to and including the last one.
+        sigma_n_1 = EWMA_SIGMA_NO_CORRELATION(df_most_recent.tail(n_volatility), EWMA_factor, n_volatility)
+
+        for i in range(0, n_scenarios):
+            
+            # Compute historical volatilities to adjust the losses
+            sigma_i = EWMA_SIGMA_NO_CORRELATION(df_most_recent.iloc[i:(n_volatility+i),:], EWMA_factor, n_volatility)
+            adjusted_loss.append(np.dot(df.iloc[i].values * (sigma_n_1 / sigma_i), allocation))
+            
+
+    if portfolio_wise:
+
+        # Give message
+        print("The program is calculating VaR and ES on portfolio-level!")
+
+        # Create a vector of dimension 8 x 1 of relative allocation. It is pct. allocated on each asset.
+        relative_allocation = np.transpose(np.matrix(allocation / np.sum(allocation)))
+
+        # Make matrix out of portfolio returns (dimension 2933 x 8).
+        pf_returns_matrix = np.matrix(df_most_recent)
+
+        # Calculate relative portfolio returns (dimension 2933 x 1)
+        pf_returns = np.dot(pf_returns_matrix, relative_allocation)
+
+        # Convert back to data frame
+        pf_returns = pd.DataFrame(pf_returns)
+        print(pf_returns)
+
+        # Then calculate E[volatility on day n + 1] = sigma_n_1. 
+        sigma_n_1 = EWMA_SIGMA_NO_CORRELATION(pf_returns.tail(n_volatility), EWMA_factor, n_volatility)
+
+        for i in range(0, n_scenarios):
+
+            # Compute historical volatilities to adjust the losses
+            sigma_i = EWMA_SIGMA_NO_CORRELATION(pf_returns.iloc[i:(n_volatility+i)], EWMA_factor, n_volatility)
+            adjusted_loss.append(pf_returns.iloc[n_volatility + i].values[0] * (sigma_n_1 / sigma_i) * total_investment)
+
+    df_results['Loss'] = adjusted_loss
+    df_results['Weights'] = weights
+
+    # Sort these by the size of the loss
+    df_sorted = df_results.sort_values(by = ['Loss'])
+    
+    # Scaling quantiles
+    quantiles = [quantile/100 for quantile in quantiles]
+    
+    # Accumulate the weights to find the right percentiles
+    accumulated_weights = [sum(df_sorted['Weights'].iloc[0:i]) for i in range(1, n_scenarios + 1)]
+    
+    df_sorted['ACC_Weights'] = accumulated_weights
+
+    # Saving the result
+    results = {}
+
+    # In order to get VaR, we find the correct index
+    for quantile in quantiles: 
+        
+        i = 0
+        
+        for i in range(0,n_scenarios-1):
+            
+            if df_sorted['ACC_Weights'].iloc[i] > (1-quantile):
+                
+                if i > 0:  
+                    results['VaR-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[i-1], 2)
+                    results['ES-' + str(int(quantile * 100))] = round(-np.mean(df_sorted['Loss'].iloc[0:i]), 2)
                     break
                     
+                else:  
+                    results['VaR-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[0], 2)
+                    results['ES-' + str(int(quantile * 100))] = round(-df_sorted['Loss'].iloc[0], 2)
+                    break
+
+    if verbose:
+        print(results)
+
     return results
-  
 
-
-
-
-def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"):
+def Backtesting(df, allocation, n_days = 252, n_volatility = 252, VaR_level = 99, paradigme = "MBA-simple", save_figure = True, figure_title = "Figure1",x_axis = []):
     
     length = df.shape[0]
     VaR = []
@@ -186,7 +467,7 @@ def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"
             VaR_estimates = Model_Based_Approach(df[(i-n_days):i], allocation, 
                                  n_days = n_days, 
                                  EWMA = False, 
-                                 lambdA = 0.94, 
+                                 lambdA = 0.994, 
                                  verbose = False, 
                                  quantiles = [VaR_level])
 
@@ -211,7 +492,7 @@ def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"
             
             VaR_estimates = Historical_Simulation(df[(i-n_days):i], allocation, 
                                          n_scenarios = n_days, 
-                                         weighted = False, 
+                                         weight_scheme = False, 
                                          lambdA = 0.94, 
                                          verbose = False, 
                                          quantiles = [VaR_level])
@@ -224,23 +505,42 @@ def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"
             
             VaR_estimates = Historical_Simulation(df[(i-n_days):i], allocation, 
                                          n_scenarios = n_days, 
-                                         weighted = True, 
+                                         weight_scheme = True, 
                                          lambdA = 0.94, 
                                          verbose = False, 
                                          quantiles = [VaR_level])
             
             VaR.append(-VaR_estimates['VaR-' + str(VaR_level)])
             
-    if paradigme == "HS-volatility-adjusted":
+    if paradigme == "HS-volatility-adjusted-asset-wise":
+        
+        for i in range(n_days+n_volatility, length):
+            
+            VaR_estimates = Historical_Simulation_Volatility_Adjusted(df[(i-(n_days+n_volatility)):i], allocation, 
+                         n_scenarios = n_days, 
+                         n_volatility = n_volatility,
+                         weight_scheme = False, 
+                         portfolio_wise = False,
+                         lambdA = 0.94, 
+                         EWMA_factor = 0.995, 
+                         verbose = False, 
+                         quantiles = [95, 99])
+            
+            VaR.append(-VaR_estimates['VaR-' + str(VaR_level)])
+
+    if paradigme == "HS-volatility-adjusted-portfolio":
         
         for i in range(n_days, length):
             
-            VaR_estimates = Historical_Simulation(df[(i-n_days):i], allocation, 
-                                         n_scenarios = n_days, 
-                                         weighted = False, 
-                                         lambdA = 0.94, 
-                                         verbose = False, 
-                                         quantiles = [VaR_level])
+            VaR_estimates = Historical_Simulation_Volatility_Adjusted(df[(i-(n_days+n_volatility)):i], allocation, 
+                         n_scenarios = n_days, 
+                         n_volatility = n_volatility,
+                         weight_scheme = False, 
+                         portfolio_wise = True,
+                         lambdA = 0.94, 
+                         EWMA_factor = 0.995,
+                         verbose = False, 
+                         quantiles = [95, 99])
             
             VaR.append(-VaR_estimates['VaR-' + str(VaR_level)])
             
@@ -286,22 +586,18 @@ def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"
     n_10 = 0
     n_11 = 0
     
-    print(violations)
-    
     for i in range(1, len(violations)):
         
-        print(violations[i-1], violations[i])
-        
-        if (violations[i-1] == 0) and (violations[i] == 0):
+        if ((violations[i-1] == 0) and (violations[i] == 0)):
             n_00 += 1
         
-        elif (violations[i-1] == 0) and (violations[i] == 1):
+        elif ((violations[i-1] == 0) and (violations[i] == 1)):
             n_01 += 1
 
-        elif (violations[i-1] == 1) and (violations[i] == 0):
+        elif ((violations[i-1] == 1) and (violations[i] == 0)):
             n_10 += 1
             
-        elif (violations[i-1] == 1) and (violations[i] == 1):
+        elif ((violations[i-1] == 1) and (violations[i] == 1)):
             n_11 += 1
             
         else:
@@ -327,15 +623,36 @@ def Backtesting(df, allocation, n_days, VaR_level = 99, paradigme = "MBA-simple"
     print("    * n_01: ", n_01)
     print("    * n_10: ", n_10)
     print("    * n_11: ", n_11)
+    print("    * Likelihood-ratio test 1 (test statistic): ", chi2_statistic_1)
     print("    * Likelihood-ratio test 1: ", p_value_1)
+    print("    * Likelihood-ratio test 2 (test statistic): ", chi2_statistic_2)
     print("    * Likelihood-ratio test 2: ", p_value_2)
+    print("    * Likelihood-ratio test 3 (test statistic): ", chi2_statistic_3)
     print("    * Likelihood-ratio test 3: ", p_value_3)
-   
-    plt.plot(VaR)
-    plt.plot(PL)
-    plt.title("Figure 1: Backtesting \n")
+
+    # Convert x-axis into right length
+    x_axis = x_axis[n_days:]
+
+    if len(x_axis) == 0:
+
+        plt.plot(VaR)
+        plt.plot(PL)
+
+    else:
+
+        plt.plot(x_axis, VaR)
+        plt.plot(x_axis, PL)
+
+    plt.xlabel('Date')
+    plt.ylabel('Profit')
+    plt.legend(['VaR-' + str(VaR_level), 'Realised returns']) 
+    #plt.title("Figure 1: Backtesting \n")
+
+    if save_figure:
+        plt.savefig(figure_title + '.svg', dpi=2400)
+
     plt.show()
-    
+
     return {'Observered violations': n_1, 'Expected violations': expected_violations}
 
     
