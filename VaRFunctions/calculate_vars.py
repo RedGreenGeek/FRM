9264ac3d16a7e4d319319cc8fdab9773090cf1a5
NicolajHMNielsen <nicolaj-mojn@hotmail.com>
2022-03-20 14:44:19 +0100

    latest changes with all approaches

--- VaRFunctions/calculate_vars.py
+++ VaRFunctions/calculate_vars.py
@@ -2,45 +2,63 @@ from VaRFunctions.VaRFunctions import *
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def calculate_vars_fx_converted(df_data, investments, alpha, market_dict, percentiles):#), forex, alpha_fx, percentiles, n=200):
-    stock_returns = df_data.loc[:, investments].pct_change().dropna()
+def calculate_vars_fx_converted(df_data, investments, alpha, alpha_linked, market_dict, fx_dict
+                            , indices_dict, base_currency, percentiles):
+    # data fx converted
+    df_fx_converted = df_data.copy()
+    df_fx_converted.loc[:,market_dict['EUR']] *= df_data.loc[:,'EURUSD=X'].values[:, np.newaxis]
+    df_fx_converted.loc[:,market_dict['GBP']] *= df_data.loc[:,'GBPUSD=X'].values[:, np.newaxis]
+    stock_returns = df_fx_converted.loc[:, investments].pct_change().dropna()
     weighted_returns = stock_returns @ alpha 
     loss = -weighted_returns
     
+    # returns for mappings:
+    df_returns = df_data.pct_change().dropna()
+    
+    # prepare output dict
     dict_output = {}
     n_calc = 250
     
     # # historical approach 
     for perc_i in percentiles:
-        hs_simple_var, es_hs_simple = HS_simple(loss, percentile=0.99, n=n_calc)
-        hs_weighted_var, es_ha_weighted = HS_weighted(loss, percentile=0.99, n=n_calc, lam=0.995)
-        mba_simple_var, es_array_mba = MBA_simple(stock_returns, alpha, percentile=0.99, n=n_calc)
-        mba_ewma_var, es_array_ewma = MBA_EWMA(stock_returns, alpha, percentile=0.99, n=n_calc, lam=0.95, sigma_init=None)
+        hs_simple_var, es_hs_simple = HS_simple(loss, percentile=perc_i, n=n_calc)
+        hs_weighted_var, es_ha_weighted = HS_weighted(loss, percentile=perc_i, n=n_calc, lam=0.995)
+        mba_simple_var, es_array_mba = MBA_simple(stock_returns, alpha, percentile=perc_i, n=n_calc)
+        mba_ewma_var, es_array_ewma = MBA_EWMA(stock_returns, alpha, percentile=perc_i, n=n_calc, lam=0.95, sigma_init=None)
         
-        dict_output[f"var_{perc_i}"] = pd.DataFrame(np.array([hs_simple_var, hs_weighted_var, mba_simple_var, mba_ewma_var]).T)
-        dict_output[f"es_{perc_i}"] = pd.DataFrame(np.array([es_hs_simple, es_ha_weighted, es_array_mba, es_array_ewma]).T)
+        # mapping approaches
+        mba_map_fx_var, es_mba_map_fx = mba_map_fx(df_returns, investments, alpha_linked, market_dict, fx_dict
+                                      , indices_dict, base_currency, percentile=perc_i, n=n_calc)
+        mba_map_index_var, es_mba_map_index = mba_map_index(df_returns, investments, alpha_linked, market_dict, fx_dict
+                                        , indices_dict, base_currency, percentile=perc_i, n=n_calc)
         
-    
+        # save results in dict for perc_i
+        dict_output[f"var_{perc_i}"] = pd.DataFrame(np.array([hs_simple_var, hs_weighted_var
+                                                              , mba_simple_var, mba_ewma_var
+                                                              , mba_map_fx_var, mba_map_index_var]).T)
+        dict_output[f"es_{perc_i}"] = pd.DataFrame(np.array([es_hs_simple, es_ha_weighted
+                                                             , es_array_mba, es_array_ewma
+                                                             , es_mba_map_fx, es_mba_map_index]).T)
     
     
     return dict_output
     
 
-def calculate_vars_risk_factors(df_data
-                                , investments
-                                , alpha_linked
-                                , market_dict
-                                , fx_dict
-                                , indices_dict
-                                , base_currency):
-    df_returns = df_data.pct_change().dropna()
+# def calculate_vars_risk_factors(df_data
+#                                 , investments
+#                                 , alpha_linked
+#                                 , market_dict
+#                                 , fx_dict
+#                                 , indices_dict
+#                                 , base_currency):
+#     df_returns = df_data.pct_change().dropna()
     
-    mba_map_fx_var, es_mba_map_fx = mba_map_fx(df_returns, investments, alpha_linked, market_dict, fx_dict
-                                      , indices_dict, base_currency, percentile=0.99, n=250)
-    mba_map_index_var, es_mba_map_index = mba_map_index(df_returns, investments, alpha_linked, market_dict, fx_dict
-                                      , indices_dict, base_currency, percentile=0.99, n=250)
+#     mba_map_fx_var, es_mba_map_fx = mba_map_fx(df_returns, investments, alpha_linked, market_dict, fx_dict
+#                                       , indices_dict, base_currency, percentile=0.99, n=250)
+#     mba_map_index_var, es_mba_map_index = mba_map_index(df_returns, investments, alpha_linked, market_dict, fx_dict
+#                                       , indices_dict, base_currency, percentile=0.99, n=250)
     
-    return pd.DataFrame([mba_map_fx_var,mba_map_index_var])
+#     return pd.DataFrame([mba_map_fx_var,mba_map_index_var])
 
 
 
--- main.py
+++ main.py
@@ -18,26 +18,17 @@ inv_market_dict = {}
 for k, v in market_dict.items():
     for x in v:
         inv_market_dict[x] = k
-df_fx_converted = df_data.copy()
-df_fx_converted.loc[:,market_dict['EUR']] *= df_data.loc[:,'EURUSD=X'].values[:, np.newaxis]
-df_fx_converted.loc[:,market_dict['GBP']] *= df_data.loc[:,'GBPUSD=X'].values[:, np.newaxis]
 
-
+# chosen stocks
 investments = ['BMW.DE', 'DWNI.DE', 'RWE.DE', 'GSK.L', 'AZN.L', 'BATS.L', 'IBM', 'MS' ]
 
+# array of initial investmens
 alpha = np.array([1000]*len(investments))
-
+# link the investments to the name for use in mapping approaches
 alpha_linked = pd.DataFrame(np.reshape(alpha,(1,len(alpha))), columns=investments)
 
-#df_fx_conv_port = df_fx_converted.loc[:,investments]
-
-#alpha_fx = np.array([10e])
-
-# var_fx_conv_df = calculate_vars_fx_converted(df_fx_converted
-#                                              , investments
-#                                              , alpha
-#                                              , market_dict
-#                                              , [0.99])
-var_loss = calculate_vars_risk_factors(df_data, investments, alpha_linked, market_dict, fx_dict
-                            , indices_dict, base_currency)
+var_fx_conv_df = calculate_vars_fx_converted(df_data, investments, alpha, alpha_linked, market_dict, fx_dict
+                            , indices_dict, base_currency, percentiles)
+# var_loss = calculate_vars_risk_factors(df_data, investments, alpha_linked, market_dict, fx_dict
+#                             , indices_dict, base_currency)
 print('lol')
